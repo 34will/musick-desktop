47f4e3e5a003f6dd62bb4b97fbd1ead08d15c80e
William Gilmour <will@nowsimplifi.com>
2018-01-01 19:36:08 +0000

    Modified Electron main file to be one file, and to run off of command line arguments. Modified ElectronWindow to optionally open DevTools.

--- electron/electron-window.js
+++ electron/electron-window.js
@@ -1,15 +1,12 @@
 const { app, BrowserWindow } = require("electron");
 
-class ElectronWindow
-{
-    constructor()
-    {
+class ElectronWindow {
+    constructor() {
         this.win = null;
         this.url = null;
     }
 
-    CreateWindow()
-    {
+    CreateWindow(devTools) {
         // Create the browser window.
         this.win = new BrowserWindow({ width: 800, height: 600 });
 
@@ -18,12 +15,12 @@ class ElectronWindow
         // and load the index.html of the app.
         this.win.loadURL(this.url);
 
-        // Open the DevTools.
-        this.win.webContents.openDevTools();
+        // Optionally open the DevTools.
+        if (devTools)
+            this.win.webContents.openDevTools();
 
         // Emitted when the window is closed.
-        this.win.on("closed", () =>
-        {
+        this.win.on("closed", () => {
             // Dereference the window object, usually you would store windows
             // in an array if your app supports multi windows, this is the time
             // when you should delete the corresponding element.
@@ -31,28 +28,27 @@ class ElectronWindow
         });
     }
 
-    Start()
-    {
+    Start(openDevTools) {
+        let devTools = openDevTools == undefined || openDevTools == null ? true : openDevTools;
+
         // This method will be called when Electron has finished
         // initialization and is ready to create browser windows.
         // Some APIs can only be used after this event occurs.
-        app.on("ready", () => this.CreateWindow());
+        app.on("ready", () => this.CreateWindow(devTools));
 
         // Quit when all windows are closed.
-        app.on("window-all-closed", () =>
-        {
+        app.on("window-all-closed", () => {
             // On macOS it is common for applications and their menu bar
             // to stay active until the user quits explicitly with Cmd + Q
             if (process.platform !== "darwin")
                 app.quit();
         });
 
-        app.on("activate", () =>
-        {
+        app.on("activate", () => {
             // On macOS it's common to re-create a window in the app when the
             // dock icon is clicked and there are no other windows open.
             if (this.win === null)
-                this.CreateWindow();
+                this.CreateWindow(devTools);
         });
 
         // In this file you can include the rest of your app's specific main process
---
+++ electron/main.js
@@ -0,0 +1,44 @@
+const ElectronWindow = require("./electron-window");
+const url = require("url");
+const path = require("path");
+
+const argumentRegex = /--(.*)=(.*)/;
+const arguments = process.argv.slice(2);
+let mode = "debug";
+let webtools = true;
+
+function ProcessArgument(argument) {
+	let matches = argument.match(argumentRegex);
+	if (matches && matches.length == 3) {
+		let option = matches[2];
+		switch (matches[1]) {
+			case "mode":
+				if (option == "release")
+					mode = option;
+				break;
+			case "webtools":
+				webtools = option === "true";
+				break;
+		}
+	}
+}
+
+process.argv.slice(2).forEach(ProcessArgument);
+
+if (mode == "debug") {
+	ElectronWindow.url = url.format({
+		pathname: "index.html",
+		hostname: "localhost",
+		protocol: "http:",
+		port: 4200
+	});
+}
+else {
+	ElectronWindow.url = url.format({
+		pathname: path.join(__dirname, "..", "src", "index.html"),
+		protocol: "file:",
+		slashes: true
+	});
+}
+
+ElectronWindow.Start(webtools);
